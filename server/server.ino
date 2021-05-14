5009b7b1a4de8cfcd1b9d0593d48249b3ef0ae84
unknown <cfRIKO>
2021-05-14 14:58:16 +0200

    Esp Servo/Motor Setup (First byte: servo, second byte: power)

--- server/server.ino
+++ server/server.ino
@@ -1,5 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <pins_arduino.h>
+#include <DBH1.h>
+#include <Servo.h>
 
 #define SSID            "ESPGOBROOM"
 #define PASSWORD        "lovestory"
@@ -7,12 +9,32 @@
 #define HIDDEN          false
 #define MAX_CONNECTIONS 1
 #define PORT            8000
-#define TIMEOUT_MS      10000
+#define TIMEOUT_MS      50
 
 WiFiServer server(PORT);
 
+#define NEUTRAL_ANGLE 93
+
+DBH1 dbh;
+Servo servo;
+
+void drive(int val) {
+  dbh.ForwardA(val);
+}
+
+void reverse(int val) {
+  dbh.ReverseA(val);
+}
+
+void steer(int angle) {
+  servo.write(NEUTRAL_ANGLE + angle)
+}
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  // Init Motor-Controller and Servo
+  dbh.init(D1, D2, D3, D4, D5, D6, A0, A0);
+  servo.attach(D8)
 
   // Default IP is 192.168.4.1.
   WiFi.softAP(SSID, PASSWORD, CHANNEL, HIDDEN, MAX_CONNECTIONS);
@@ -29,6 +51,8 @@ void loop() {
 
   // Turn LED *off* after client connected.
   digitalWrite(LED_BUILTIN, HIGH);
+  // Enable Motor
+  dbh.EnableA();
 
   client.setTimeout(TIMEOUT_MS);
   while (client.connected()) {
@@ -37,11 +61,15 @@ void loop() {
 
     if (num_bytes_rec != sizeof(msg)) {
       // TIMEOUT OCCURED.
-      client.stop();
-      // TODO: Stop engine.
+      drive(0);
+      steer(0)
     } else {
-      // Echo the received message.
-      client.write(msg, sizeof(msg));
+      int8_t steeringAngle = msg[0];
+      int8_t power = msg[1];
+      steer(steeringAngle);
+      if (power >= 0) { drive(floor(970*power/127.0f)); }
+      else { reverse(floor(970*power/-128.0)); }
     }
   }
+  dbh.DisableA();
 }
